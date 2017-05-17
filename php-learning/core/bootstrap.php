dcbd75a0d3126f7ca55242b384d2a054baac985c
MayLiu <ashucn@gmail.com>
2017-05-16 23:56:23 -0700

    update
    Section 22

--- php-learning/core/bootstrap.php
+++ php-learning/core/bootstrap.php
@@ -1,15 +1,11 @@
 <?php 
 // Global db Statement
-// 
-$app = [];
+App::bind('config', require 'config.php');
+$config = App::get('config');
+// die(var_dump(App::get('config')));
 
-$app['config'] = require 'config.php';
-// 使用composer, 定义了autoload的classmap，以下4行就不需要要了
-/*require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';*/
-
-$app['database'] = new QueryBuilder(
-	Connection::make($app['config']['database'])
+App::bind('database', 
+	new QueryBuilder(
+		Connection::make($config['database'])
+	)
 );
